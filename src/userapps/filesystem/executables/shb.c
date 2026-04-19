bde96ec346786ce64ef5c84234d48c6539c22eaf
bufferbird <270710080+bufferbird@users.noreply.github.com>
2026-04-19 16:29:59 +0200

    Add SHB echo, alert, and exit functions

--- src/userapps/filesystem/executables/shb.c
+++ src/userapps/filesystem/executables/shb.c
@@ -1,1 +1,12 @@
+void shb_echo(char* arg) {
+    if (arg) kprintf("%s\n", arg);
+}
 
+void shb_alert(char* arg) {
+    // Hier käme dein !alert id = warnbox Logik rein
+    kprintf("[SHB-SECURITY] ALERT TRIGGERED: %s\n", arg ? arg : "Generic Error");
+}
+
+void shb_exit(char* arg) {
+    kprintf("Leaving SHB... bye loser.\n");
+}

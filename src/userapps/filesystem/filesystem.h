113542c0eb8e26a6378ffb1c69f34b29b07870e5
bufferbird <270710080+bufferbird@users.noreply.github.com>
2026-04-20 20:04:03 +0200

    Add filesystem header with file and folder functions

--- src/userapps/filesystem/filesystem.h
+++ src/userapps/filesystem/filesystem.h
@@ -1,1 +1,10 @@
+#ifndef FILESYSTEM_H
+#define FILESYSTEM_H
 
+void save_file(long long folder, long long filename);
+void create_folder(long long folder, long long foldername);
+void create_file(long long filename, long long foldername);
+void open_file(longlong );
+void navigate_trough_filesystem();
+
+#endif
